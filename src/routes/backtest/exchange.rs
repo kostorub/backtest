e450f99c35c6da91e412437b8e058fd3be336f7d
Vasilii Kostrubov <vs.kostrubov@gmail.com>
2023-10-08 16:27:58 +0300

    added cached

--- src/routes/backtest/exchange.rs
+++ src/routes/backtest/exchange.rs
@@ -4,6 +4,8 @@ use actix_web::{
     HttpResponse,
 };
 use tera::Context;
+use cached::proc_macro::cached;
+
 
 pub async fn exchanges(data: web::Data<AppState>) -> HttpResponse {
     let exchanges: Vec<String> = vec!["Binance".into()];
@@ -40,7 +42,6 @@ pub async fn symbols(data: web::Data<AppState>) -> HttpResponse {
     HttpResponse::Ok().body(body)
 }
 
-// TODO add cached
 pub async fn exchange_symbols(data: web::Data<AppState>, path: Path<(String,)>) -> HttpResponse {
     let exchange_name = path.into_inner().0;
 
@@ -49,7 +50,7 @@ pub async fn exchange_symbols(data: web::Data<AppState>, path: Path<(String,)>) 
         _ => return HttpResponse::BadRequest().body("Unknown exchange"),
     };
 
-    let body = reqwest::get(url).await.unwrap().text().await.unwrap();
+    let body = get_symbols(url.to_string()).await;
 
     let json_body: serde_json::Value = serde_json::from_str(&body).unwrap();
 
@@ -69,6 +70,12 @@ pub async fn exchange_symbols(data: web::Data<AppState>, path: Path<(String,)>) 
     let body = tera.render("select_options.html", &context).unwrap();
 
     HttpResponse::Ok().body(body)
+}
+
+
+#[cached(time = 86400)]
+pub async fn get_symbols(url: String) -> String {
+    reqwest::get(url).await.unwrap().text().await.unwrap()
 }
 
 pub async fn market_data_types(data: web::Data<AppState>) -> HttpResponse {
