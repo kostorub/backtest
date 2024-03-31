9efb032459e9307382ec4f5225e7e92c43c9fb63
Vasilii Kostrubov <vs.kostrubov@gmail.com>
2024-03-31 21:15:57 +0300

    env::var("ENV")

--- src/config.rs
+++ src/config.rs
@@ -10,8 +10,10 @@ pub struct AppSettings {
 
 impl AppSettings {
     pub fn new() -> Result<Self, ConfigError> {
+        let env = std::env::var("ENV").unwrap_or("local".into());
+        let config_file = format!("config/{}", env.to_lowercase());
         let s = Config::builder()
-            .add_source(File::with_name("config/local").required(true))
+            .add_source(File::with_name(&config_file).required(true))
             .add_source(Environment::default())
             .build()?;
 
