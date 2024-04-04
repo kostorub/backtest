9059a2e7adf52e9dbfb8e772e368c38a8949f4d0
Vasilii Kostrubov <vs.kostrubov@gmail.com>
2024-04-04 20:00:52 +0300

    removed unnecessary routes

--- src/routes/backtest/mod.rs
+++ src/routes/backtest/mod.rs
@@ -2,5 +2,4 @@ pub mod backtest;
 pub mod backtest_result;
 pub mod exchange;
 pub mod market_data;
-pub mod pages;
 pub mod utils;
--- src/routes/backtest_ui/mod.rs
+++ src/routes/backtest_ui/mod.rs
@@ -1,5 +1,4 @@
 pub mod backtest;
-pub mod backtest_result;
 pub mod exchange;
 pub mod market_data;
 pub mod pages;
