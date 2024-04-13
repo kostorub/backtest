d367695392f5e7fd55d78a6300d9b15dcb020166
Vasilii Kostrubov <vs.kostrubov@gmail.com>
2024-04-13 23:56:22 +0300

    Cors::default

--- src/server.rs
+++ src/server.rs
@@ -50,7 +50,7 @@ pub async fn start_server() -> std::io::Result<()> {
     });
 
     HttpServer::new(move || {
-    let cors = Cors::permissive()
+    let cors = Cors::default()
         .allow_any_origin()
         .allow_any_method()
         .allow_any_header()
